ad8d9e15376d6f207b3f56ef53e9a6b442324f92
unknown <ty202500691@wmsu.edu.ph>
2026-07-22 22:41:37 +0800

    Muliply Two Added Numbers

---
+++ Basic Problems/Subtract Two Numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, difference;
+
+    cout<<"Enter first number: ";
+    cin>>num1;
+
+    cout<<"Enter second number: ";
+    cin>>num2;
+
+    difference = num1 - num2;
+
+    cout<<"The difference is: "<< difference;
+
+return 0;
+}
