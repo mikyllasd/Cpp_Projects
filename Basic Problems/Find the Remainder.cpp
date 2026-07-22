ee9537c4884dbc7160dbc9ae9939b66c355dc428
unknown <ty202500691@wmsu.edu.ph>
2026-07-22 23:00:34 +0800

    Find the Remainder

---
+++ Basic Problems/Find the Remainder.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, remainder;
+
+    cout<<"Enter first number: ";
+    cin>>num1;
+
+    cout<<"Enter second numebr: ";
+    cin>>num2;
+
+    remainder =  num1 % num2;
+
+    cout<<"The remainder is: "<<remainder;
+
+return 0;
+}
