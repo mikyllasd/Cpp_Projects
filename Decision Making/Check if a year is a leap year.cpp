559ea80a2e8064b1ffcff4112ea79ef010a1c595
unknown <ty202500691@wmsu.edu.ph>
2026-07-23 13:40:43 +0800

    Check if a year is a leap year

---
+++ Decision Making/Check if a year is a leap year.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int year;
+
+    cout<<"Enter year";
+    cin>>year;
+
+    if(year % 400 == 0){
+        cout<<"leap year";
+    }else if(year % 4 == 0 && year % 100 != 0){
+        cout<<"leap year";
+    }else{
+        cout<<"not a leap year";
+    }
+
+return 0;
+}
