147be45a46c0f4e8138e6eaa026a12c899f12a31
unknown <ty202500691@wmsu.edu.ph>
2026-07-23 12:27:23 +0800

    Calculate the Area of a Rectangle

---
+++ Basic Problems/Calculate the Area of a Rectangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    double length, width, area;
+
+    cout<<"Enter length: ";
+    cin>>length;
+
+    cout<<"Enter width: ";
+    cin>>width;
+
+    area = length * width;
+
+    cout<<"The area of a rectangle is: "<<area;
+
+return 0;
+}
