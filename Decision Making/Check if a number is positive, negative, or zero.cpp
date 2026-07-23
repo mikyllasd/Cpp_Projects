03ef8db73df36ba7c8e6123e7ee58b5e0759119f
unknown <ty202500691@wmsu.edu.ph>
2026-07-23 13:09:45 +0800

    Check if a number is positive, negative, or zero

---
+++ Decision Making/Check if a number is positive, negative, or zero.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    cout<<"Enter number: ";
+    cin>>num;
+
+    if(num > 0){
+        cout<<"positive number";
+    }else if(num < 0){
+        cout<<"negative number";
+    }else {
+        cout<<"zero";
+    }
+
+return 0;
+}
